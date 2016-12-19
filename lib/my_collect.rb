8c9bef3929ecfb707c7d25fc19b0bcdf3f6da043
Maria Ramirez <nanyramirez0@gmail.com>
2016-12-19 23:23:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+  i=0
+  returned_array=[]
+  while i<collection.length
+    returned_array.push(yield collection[i])
+    i+=1
+  end
+  returned_array
+end
 
-
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
+  name.split(" ").first
+end
